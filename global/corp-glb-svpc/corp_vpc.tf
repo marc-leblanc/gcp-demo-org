53939b5748258928bf15bd422159c2bce4d9101f
Marc LeBlanc <mleblanc@localhost.localdomain>
2020-10-21 12:57:43 -0400

    initial corp vpc tf

---
+++ global/corp-glb-svpc/corp_vpc.tf
@@ -0,0 +1,7 @@
+module "vpc" {
+  source  = "app.terraform.io/leblanchq/vpc/gcp"
+  version = "1.0.0"
+  # insert required variables here
+  gcp_vpc_name = var.corp_vpc_name
+  gcp_vpc_description = var.corp_vpc_description
+}
---
+++ global/corp-glb-svpc/variables.tf
@@ -0,0 +1,7 @@
+variable "gcp_vpc_name" {
+    description = "VPC Name"
+}
+
+variable "gcp_vpc_description"{
+    default = "GCP VPC Network"
+}
