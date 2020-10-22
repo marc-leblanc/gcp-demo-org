86a3bc668ce57ef1ed045a0ed20e9b3f21743bd6
Marc LeBlanc <mleblanc@localhost.localdomain>
2020-10-22 14:14:36 -0400

    test

--- country/ca/ca_subnets.tf
+++ country/ca/ca_subnets.tf
@@ -2,5 +2,5 @@ data "google_compute_subnetwork" "ca_subnet_01" {
   name   = var.snet_name
   region = var.gcp_region
   ip_cidr_range = var.ip_cidr_range
-  network = data.terraform_remote_state.corp_vpc.outputs.vpc_name
+  #network = data.terraform_remote_state.corp_vpc.outputs.vpc_name
 }
