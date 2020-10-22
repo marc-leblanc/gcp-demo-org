1644977eec0c24f05fea46ba8fa87060ecfd59a9
Marc LeBlanc <mleblanc@localhost.localdomain>
2020-10-22 14:19:35 -0400

    doh - resource not data

--- country/ca/ca_subnets.tf
+++ country/ca/ca_subnets.tf
@@ -1,6 +1,6 @@
-data "google_compute_subnetwork" "ca_subnet_01" {
+resource "google_compute_subnetwork" "ca_subnet_01" {
   name   = var.snet_name
   region = var.gcp_region
-  ip_cidr_range = "10.10.0.0/16"
-  #network = data.terraform_remote_state.corp_vpc.outputs.vpc_name
+  ip_cidr_range = var.ip_cird_range
+  network = data.terraform_remote_state.corp_vpc.outputs.vpc_name
 }
