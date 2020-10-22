15a1357415a376be45aa78631274d4293eecc26e
Marc LeBlanc <mleblanc@localhost.localdomain>
2020-10-22 14:20:06 -0400

    typo

--- country/ca/ca_subnets.tf
+++ country/ca/ca_subnets.tf
@@ -1,6 +1,6 @@
 resource "google_compute_subnetwork" "ca_subnet_01" {
   name   = var.snet_name
   region = var.gcp_region
-  ip_cidr_range = var.ip_cird_range
+  ip_cidr_range = var.ip_cidr_range
   network = data.terraform_remote_state.corp_vpc.outputs.vpc_name
 }
