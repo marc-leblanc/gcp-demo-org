8051232f452d6d5208d55e59fa04650e7135b7ba
Marc LeBlanc <mleblanc@localhost.localdomain>
2020-10-26 16:34:48 -0400

    remove CA Subnet and see what happens

--- country/ca/ca_subnets.tf
+++ country/ca/ca_subnets.tf
@@ -1,9 +1,9 @@
-resource "google_compute_subnetwork" "ca_subnet_01" {
-  name   = var.snet_name
-  region = var.gcp_region
-  ip_cidr_range = var.ip_cidr_range
-  network = data.terraform_remote_state.corp_vpc.outputs.vpc_name
-}
+#resource "google_compute_subnetwork" "ca_subnet_01" {
+#  name   = var.snet_name
+#  region = var.gcp_region
+#  ip_cidr_range = var.ip_cidr_range
+#  network = data.terraform_remote_state.corp_vpc.outputs.vpc_name
+#}
 
 output "ca_subnet_01" { 
   value = google_compute_subnetwork.ca_subnet_01
