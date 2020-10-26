30e3a2fa902bcb74e3cb52afebc3958653c7b664
Marc LeBlanc <mleblanc@localhost.localdomain>
2020-10-26 12:08:27 -0400

    remove gcp project

--- lob/sprockets/compute.tf
+++ lob/sprockets/compute.tf
@@ -2,7 +2,7 @@ module "compute-instance" {
   source  = "app.terraform.io/leblanchq/compute-instance/google"
   version = "1.0.2"
   svpc_project_name = var.gcp_project
-  subnet = data.terraform_remote_state.lhq_ca_network.ca_subnet_01.name
+  subnet = data.terraform_remote_state.lhq_ca_network["ca_subnet_01.name"]
   node_count = 1
   data_disk_count = 1 
   host_prefix = "lhq_sprockets_"
