d0d65e2a342987fcd09dffe1237c14508b705d33
Marc LeBlanc <mleblanc@localhost.localdomain>
2020-10-26 12:37:15 -0400

    new mod/label map

--- lob/sprockets/compute.tf
+++ lob/sprockets/compute.tf
@@ -1,10 +1,10 @@
 module "compute-instance" {
   source  = "app.terraform.io/leblanchq/compute-instance/google"
-  version = "1.0.6"
+  version = "1.0.7"
   svpc_project_name = var.gcp_project
   subnet = data.terraform_remote_state.lhq_ca_network.outputs.ca_subnet_01["name"]
   node_count = 1
   data_disk_count = 1 
   host_prefix = "lhq_sprockets_"
-  labels = "sprockets,ca"
+  labels = { "lob"="sprockets", "country"="ca"}
 }
