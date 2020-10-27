9e71eb31680c860aae9b4136c13b5d028b0053db
Marc LeBlanc <mleblanc@localhost.localdomain>
2020-10-27 09:58:33 -0400

    project id update

--- project-control/lhq-prod-projectXYZ.tf
+++ project-control/lhq-prod-projectXYZ.tf
@@ -1,5 +1,5 @@
 resource "google_project" "lhq_prod_project" {
   name       = "lhq-prod-projectXYZ"
-  project_id = "lhq-prod-projectXYZ-id"
+  project_id = "lhq-prod-projectxyz-id"
   folder_id  = var.gcp_folder_id
 }
