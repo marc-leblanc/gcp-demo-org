6c4c57661cefe679d6075fedf603521c5ff52777
Marc LeBlanc <mleblanc@localhost.localdomain>
2020-10-27 12:11:56 -0400

    billing for projects

--- project-control/lhq-project_cp1.tf
+++ project-control/lhq-project_cp1.tf
@@ -2,6 +2,7 @@ resource "google_project" "lhq_project_cp1" {
   name       = var.gcp_project_name
   project_id = "${var.gcp_project_name}-id"
   folder_id  = var.gcp_folder_id
+  billing_account = var.gcp_billing_account
 
   auto_create_network = false
 
--- project-control/variables.tf
+++ project-control/variables.tf
@@ -1,2 +1,3 @@
 variable "gcp_folder_id"{}
 variable "gcp_project_name" {}
+variable "gcp_billing_account" {}
